c2baa351b003dd1e77dd49cd0454c0f96cbd2919
danielle <danielle.costa@somosquiq.com>
2022-11-23 11:06:17 -0300

    arquivos finais da livraria

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -55,6 +55,12 @@ static apagarLivro = (req, res) => {
         }
     })
 }
+static buscarLivroEditora = (req, res) => {
+    const editora= req.query.editora
+    livros.find({'editora': editora}, {}, (err, livros) =>{
+        res.status(200).send(livros)
+    })
+}
 
 }
 export default LivroController
